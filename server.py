51e8d29321c908e771b1e6e0243639a07eed0568
qq767968585 <767968585@qq.com>
2020-11-27 09:05:40 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,5 +4,5 @@ from app import app
 from tornado.ioloop import IOLoop
 
 s = HTTPServer(WSGIContainer(app))
-s.listen(5000)             # 监听 5000 端口，默认 5000 端口
+s.listen(5000)             # 监听 5000 端口，flask默认 5000 端口
 IOLoop.current().start()
